f7969f0d151fae547b61c82a7208cd3c7914339b
joe ling <josephling11@gmail.com>
2014-12-29 20:44:09 +0000

    test

--- src/main/java/com/Enigma/SM/worldgen/structures/Dungeon.java
+++ src/main/java/com/Enigma/SM/worldgen/structures/Dungeon.java
@@ -9,6 +9,7 @@ import cpw.mods.fml.common.IWorldGenerator;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntityMobSpawner;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 
 import java.util.Random;
@@ -30,9 +31,9 @@ public class Dungeon implements IWorldGenerator
         int Zchunk = chunkZ *15;
         int Y = ConfiguartionHandler.DG_YValue;
         int yHeight = 6;
-        if (Xchunk % 100 == 0 && Zchunk % 100 == 0 && random.nextInt(2) == 0)
+        if (Xchunk % 100 == 0 && Zchunk % 100 == 0 && random.nextInt(2) == 0 && world.provider.dimensionId == 0 && world.getBiomeGenForCoords(Xchunk, Zchunk) != BiomeGenBase.ocean && world.getBiomeGenForCoords(Xchunk, Zchunk) != BiomeGenBase.deepOcean && world.getBiomeGenForCoords(Xchunk, Zchunk) != BiomeGenBase.extremeHills && world.getBiomeGenForCoords(Xchunk, Zchunk) != BiomeGenBase.extremeHillsPlus && world.getBiomeGenForCoords(Xchunk, Zchunk) != BiomeGenBase.extremeHillsEdge)
         {
-
+            long StartTime = System.nanoTime();
             LogHelper.info("generated dungoen");
             char[][] Map = DungeonGenerator.genMap(random, ConfiguartionHandler.DG_XSIZE, ConfiguartionHandler.DG_ZSIZE, ConfiguartionHandler.DG_nTRIES);
             // 50, 50, 250
@@ -67,148 +68,138 @@ public class Dungeon implements IWorldGenerator
             {
                 for (int X = 0; X <Map[0].length ; X++)
                 {
-                    if (Map[Z][X] == '+')
+                    switch(Map[Z][X])
                     {
-                        for (int i = 1; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, ModBlocks.ExampleBlock, 0, 2);
-
-                        }
-                    }
-                    else if (Map[Z][X] == '#')
-                    {
-                        if (random.nextInt(3) == 0)
-                        {
-                            world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.mossy_cobblestone, 0, 2);
-
-                        }
-                        else
-                        {
-                            world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
-                        }
-                        for (int i = 1; i < yHeight-1; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
-
-                        }
-
-                    }
-                    else if (Map[Z][X] == 'w')
-                    {
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            int randChoice = random.nextInt(5);
-                            if (randChoice == 0)
+                        case '+':
+                            for (int i = 1; i < yHeight; i++)
                             {
-                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 1, 2); // mossy stone brick
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, ModBlocks.ExampleBlock, 0, 2);
+
                             }
-                            else if(randChoice == 1)
+                            break;
+
+                        case '#':
+                            if (random.nextInt(3) == 0)
                             {
-                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 3, 2); // chiseled stone brick
+                                world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.mossy_cobblestone, 0, 2);
+
                             }
                             else
                             {
-                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 0, 2);
+                                world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
                             }
-
-                        }
-
-                    }
-                    else if (Map[Z][X] == 'b')
-                    {
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.bookshelf, 0, 2);
-
-                        }
-                    }
-                    else if (Map[Z][X] == 'c')
-                    {
-                        world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
-                        world.setBlock(Xchunk + X, Y+1, Zchunk + Z, ModBlocks.LootChestBlock, 0, 2);
-
-                        if (random.nextInt(2) == 0)
-                        {
-                            world.setBlock(Xchunk + X, Y+2, Zchunk + Z, ModBlocks.LootChestBlock, 0, 2);
-                            for (int i = 2; i < yHeight-2; i++)
+                            for (int i = 1; i < yHeight-1; i++)
                             {
                                 world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
 
                             }
-                        }
-                        else
-                        {
+                            break;
+
+                        case 'w':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                int randChoice = random.nextInt(5);
+                                if (randChoice == 0)
+                                {
+                                    world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 1, 2); // mossy stone brick
+                                }
+                                else if(randChoice == 1)
+                                {
+                                    world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 3, 2); // chiseled stone brick
+                                }
+                                else
+                                {
+                                    world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 0, 2);
+                                }
+
+                            }
+                            break;
+
+                        case 'b':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.bookshelf, 0, 2);
+
+                            }
+                            break;
+
+                        case 'c':
+                            world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
+                            world.setBlock(Xchunk + X, Y+1, Zchunk + Z, ModBlocks.LootChestBlock, 0, 2);
+
+                            if (random.nextInt(2) == 0)
+                            {
+                                world.setBlock(Xchunk + X, Y+2, Zchunk + Z, ModBlocks.LootChestBlock, 0, 2);
+                                for (int i = 3; i < yHeight-2; i++)
+                                {
+                                    world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
+
+                                }
+                            }
+                            else
+                            {
+                                for (int i = 2; i < yHeight-1; i++)
+                                {
+                                    world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
+
+                                }
+                            }
+                            break;
+
+                        case 's':
+                            world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
+                            world.setBlock(Xchunk + X, Y+1, Zchunk + Z, Blocks.mob_spawner, 0, 2);
+                            TileEntityMobSpawner Spawner = (TileEntityMobSpawner)world.getTileEntity(Xchunk + X, Y+1, Zchunk + Z);
+                            Spawner.func_145881_a().setEntityName("Zombie");
                             for (int i = 2; i < yHeight-1; i++)
                             {
                                 world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
 
                             }
-                        }
+                            break;
 
+                        case 'j':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, ModBlocks.ExampleBlock, 1, 2);
+                            }
+                            break;
 
-                    }
-                    else if (Map[Z][X] == 's')
-                    {
-                        world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.cobblestone, 0, 2);
-                        world.setBlock(Xchunk + X, Y+1, Zchunk + Z, Blocks.mob_spawner, 0, 2);
-                        TileEntityMobSpawner Spawner = (TileEntityMobSpawner)world.getTileEntity(Xchunk + X, Y+1, Zchunk + Z);
-                        Spawner.func_145881_a().setEntityName("Zombie");
-                        for (int i = 2; i < yHeight-1; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.air, 0, 2);
+                        case 'f':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 2, 2);
+                            }
+                            break;
 
-                        }
+                        case 'i':
+                            world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.stonebrick, 3, 2); // chiseled stone brick
+                            for (int i = 1; i < yHeight-1; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.iron_bars, 2, 2);
+                            }
+                            world.setBlock(Xchunk + X, Y + yHeight-1, Zchunk + Z, ModBlocks.ExampleBlock,0,  2);
+                            break;
 
-                    }
-                    else if (Map[Z][X] == 'j')
-                    {
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, ModBlocks.ExampleBlock, 1, 2);
-                        }
-                    }
-                    else if (Map[Z][X] == 'f')
-                    {
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.stonebrick, 2, 2);
-                        }
-                    }
-                    else if (Map[Z][X] == 'i')
-                    {
-                        world.setBlock(Xchunk + X, Y, Zchunk + Z, Blocks.stonebrick, 3, 2); // chiseled stone brick
-                        for (int i = 1; i < yHeight-1; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.iron_bars, 2, 2);
-                        }
-                        world.setBlock(Xchunk + X, Y + yHeight-1, Zchunk + Z, ModBlocks.ExampleBlock,0,  2);
-                    }
+                        // user for debugging this stuff below here isL:
+                        case 'd':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.diamond_ore, 0, 2);
+                            }
+                            break;
 
+                        case 'a':
+                            for (int i = 0; i < yHeight; i++)
+                            {
+                                world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.sandstone, 0, 2);
 
-                    // user for debugging this stuff below here isL:
-                    else if (Map[Z][X] == 'd')
-                    {
-
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.diamond_ore, 0, 2);
-                        }
-
-
-                    }
-                    else if (Map[Z][X] == 'a')
-                    {
-                        for (int i = 0; i < yHeight; i++)
-                        {
-                            world.setBlock(Xchunk + X, Y+i, Zchunk + Z, Blocks.sandstone, 0, 2);
-
-                        }
-
+                            }
+                            break;
                     }
                 }
             }
-
-
+            LogHelper.info("took: " + (StartTime - System.nanoTime()) + " seconds");
         }
     }
 
